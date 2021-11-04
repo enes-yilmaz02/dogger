002608cdcc3c5bfee8209f1108ab971d6e3acea7
enes yilmaz <ey684241@gmail.com>
2021-11-04 13:58:46 +0300

    working

--- egitim.js
+++
@@ -1,9 +0,0 @@
-const card = document.querySelector(".card");
-
-card.onclick = function() {
-  if (card.className == "card") {
-    card.className = "card full";  
-  } else {
-    card.className = "card";
-  }
-}
